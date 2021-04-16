e977acd486b1c3f2e6687ccc7102ad7fb0a62162
cesar augusto <djkav2009@gmail.com>
2021-04-16 09:00:09 -0500

    se agrega parametro

--- src/main/java/com/ias/handyman/servicereport/TestRepository.java
+++ src/main/java/com/ias/handyman/servicereport/TestRepository.java
@@ -2,6 +2,7 @@ package com.ias.handyman.servicereport;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -9,5 +10,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource(collectionResourceRel = "test", path = "test")
 @CrossOrigin
 public interface TestRepository extends MongoRepository<Test, String> {
-
+    List<Test> findByEmployeeDocumentNumber(@Param("employeeDocumentNumber") String name);
 }
