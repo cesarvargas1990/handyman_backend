e53cafe55d279349f4d3cfb652c3e24e63c42703
cesar vargas <cesara.vargas1990@gmail.com>
2021-04-21 12:12:56 -0500

    se agregan test

--- src/test/java/com/ias/handyman/report/service/ReportServiceTest.java
+++ src/test/java/com/ias/handyman/report/service/ReportServiceTest.java
@@ -1,60 +1,51 @@
 package com.ias.handyman.report.service;
 
+import com.ias.handyman.report.entity.Report;
+import com.ias.handyman.report.repository.ReportRepository;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-//import static org.mockito.Mockito.*;
+
+import java.util.*;
+
+import static org.mockito.Mockito.*;
+
 class ReportServiceTest {
     @Mock
-    com.ias.handyman.report.repository.ReportRepository reportRepository;
+    ReportRepository reportRepository;
     @InjectMocks
-    com.ias.handyman.report.service.ReportService reportService;
+    ReportService reportService;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
     }
 
+
     @Test
-    void testConsultarTodos(){
-        java.util.List<com.ias.handyman.report.entity.Report> result = reportService.consultarTodos();
-        Assertions.assertEquals(java.util.Arrays.<com.ias.handyman.report.entity.Report>asList(new com.ias.handyman.report.entity.Report()), result);
+    void testSubtactNumberDays() {
+        Integer result = reportService.subtactNumberDays(Integer.valueOf(200));
+        Assertions.assertEquals(Integer.valueOf(152), result);
     }
 
     @Test
-    void testSearchByWeekAndIdentify(){
-        when(reportRepository.findByEmployeeDocumentNumber(anyString())).thenReturn(java.util.Arrays.<com.ias.handyman.report.entity.Report>asList(new com.ias.handyman.report.entity.Report()));
-
-        java.util.Map<java.lang.String,java.lang.Integer> result = reportService.searchByWeekAndIdentify(new com.ias.handyman.report.entity.Report());
-        Assertions.assertEquals(new java.util.HashMap<java.lang.String,java.lang.Integer>(){{put("String",Integer.valueOf(0));}}, result);
-    }
-
-    @Test
-    void testSubtactNumberDays(){
-        java.lang.Integer result = reportService.subtactNumberDays(Integer.valueOf(0));
+    void testSubtractTwoHoursTotal() {
+        Integer result = reportService.subtractTwoHoursTotal(Integer.valueOf(0), Integer.valueOf(0));
         Assertions.assertEquals(Integer.valueOf(0), result);
     }
 
     @Test
-    void testSubtractTwoHoursTotal(){
-        java.lang.Integer result = reportService.subtractTwoHoursTotal(Integer.valueOf(0), Integer.valueOf(0));
-        Assertions.assertEquals(Integer.valueOf(0), result);
-    }
-
-    @Test
-    void testDiffBetweenHours(){
-        java.lang.Long result = reportService.diffBetweenHours(new java.util.GregorianCalendar(2021, java.util.Calendar.APRIL, 21, 11, 39).getTime(), new java.util.GregorianCalendar(2021, java.util.Calendar.APRIL, 21, 11, 39).getTime());
+    void testDiffBetweenHours() {
+        Long result = reportService.diffBetweenHours(new Date(2021, 4, 21, 11, 39), new Date(2021, 4,21,12,39));
         Assertions.assertEquals(Long.valueOf(1), result);
     }
 
     @Test
-    void testIncrementHours(){
-        java.lang.Integer result = reportService.incrementHours(Integer.valueOf(0), Integer.valueOf(0));
+    void testIncrementHours() {
+        Integer result = reportService.incrementHours(Integer.valueOf(0), Integer.valueOf(0));
         Assertions.assertEquals(Integer.valueOf(0), result);
     }
 }
-
-//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
